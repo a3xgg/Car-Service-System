4109c76351a23a42f7be62a6ffdeb486cb3fe3d8
a3xgg <notandersonfebrian@gmail.com>
2019-12-26 22:05:22 +0800

    nothing much

--- src/gui/TechMenu.java
+++ src/gui/TechMenu.java
@@ -3,11 +3,16 @@ package gui;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class TechMenu extends JFrame implements ActionListener{
     
+    private JButton checkSchedule;
+    
     public TechMenu(){
+        checkSchedule = new JButton("Check Appointment");
+        checkSchedule.addActionListener(this);
         setSize(200,200);
         setLocation(200,100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,7 +21,6 @@ public class TechMenu extends JFrame implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
