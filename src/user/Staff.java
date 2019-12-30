2d21c1f591e7a773e0f587cb1f8b5256aaa6899c
a3xgg <notandersonfebrian@gmail.com>
2019-12-31 00:34:14 +0800

    removed arraylist
    kind of doesn't make sense to use array list and assigning each staff with an account when you can assigned the staff with account within the account file

--- src/user/Staff.java
+++ src/user/Staff.java
@@ -1,24 +1,11 @@
 package user;
 
-import java.util.ArrayList;
-
 public class Staff extends Person{
     
     private String department;
-    private ArrayList<StaffAccount> staffAccount;
-    
     public Staff(String name, String icNumber, String email, String phoneNumber, String mailingAddress, String department) {
         super(name, icNumber, email, phoneNumber, mailingAddress);
         this.department = department;
-        staffAccount = new ArrayList<StaffAccount>();
-    }
-
-    public ArrayList<StaffAccount> getStaffAccount() {
-        return staffAccount;
-    }
-    
-    public void addStaffAccount(StaffAccount staffAccount){
-        this.staffAccount.add(staffAccount);
     }
 
     public String getDepartment() {
